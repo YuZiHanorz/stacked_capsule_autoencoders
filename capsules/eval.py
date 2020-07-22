783554f0e9ce86843a4fdbc8539aad3ffd631b0f
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2020-07-22 23:04:18 +0800

    try profile with timeline

--- capsules/eval.py
+++ capsules/eval.py
@@ -26,68 +26,81 @@ from monty.collections import AttrDict
 import numpy as np
 from scipy.optimize import linear_sum_assignment
 import sklearn.cluster
-
+import tensorflow as tf
+from tensorflow.python.client import timeline
 
 def bipartite_match(pred, gt, n_classes=None, presence=None):
-  """Does maximum biprartite matching between `pred` and `gt`."""
+    """Does maximum biprartite matching between `pred` and `gt`."""
 
-  if n_classes is not None:
-    n_gt_labels, n_pred_labels = n_classes, n_classes
-  else:
-    n_gt_labels = np.unique(gt).shape[0]
-    n_pred_labels = np.unique(pred).shape[0]
+    if n_classes is not None:
+        n_gt_labels, n_pred_labels = n_classes, n_classes
+    else:
+        n_gt_labels = np.unique(gt).shape[0]
+        n_pred_labels = np.unique(pred).shape[0]
 
-  cost_matrix = np.zeros([n_gt_labels, n_pred_labels], dtype=np.int32)
-  for label in range(n_gt_labels):
-    label_idx = (gt == label)
-    for new_label in range(n_pred_labels):
-      errors = np.equal(pred[label_idx], new_label).astype(np.float32)
-      if presence is not None:
-        errors *= presence[label_idx]
+    cost_matrix = np.zeros([n_gt_labels, n_pred_labels], dtype=np.int32)
+    for label in range(n_gt_labels):
+        label_idx = (gt == label)
+        for new_label in range(n_pred_labels):
+            errors = np.equal(pred[label_idx], new_label).astype(np.float32)
+            if presence is not None:
+                errors *= presence[label_idx]
 
-      num_errors = errors.sum()
-      cost_matrix[label, new_label] = -num_errors
+            num_errors = errors.sum()
+            cost_matrix[label, new_label] = -num_errors
 
-  row_idx, col_idx = linear_sum_assignment(cost_matrix)
-  num_correct = -cost_matrix[row_idx, col_idx].sum()
-  acc = float(num_correct) / gt.shape[0]
-  return AttrDict(assingment=(row_idx, col_idx), acc=acc,
-                  num_correct=num_correct)
+    row_idx, col_idx = linear_sum_assignment(cost_matrix)
+    num_correct = -cost_matrix[row_idx, col_idx].sum()
+    acc = float(num_correct) / gt.shape[0]
+    return AttrDict(assingment=(row_idx, col_idx),
+                    acc=acc,
+                    num_correct=num_correct)
 
 
 def cluster_classify(features, gt_label, n_classes, kmeans=None, max_iter=100):
-  """Performs clustering and evaluates it with bipartitate graph matching."""
-  if kmeans is None:
-    kmeans = sklearn.cluster.KMeans(
-        n_clusters=n_classes,
-        precompute_distances=True,
-        n_jobs=-1,
-        max_iter=max_iter,
-    )
+    """Performs clustering and evaluates it with bipartitate graph matching."""
+    if kmeans is None:
+        kmeans = sklearn.cluster.KMeans(
+            n_clusters=n_classes,
+            precompute_distances=True,
+            n_jobs=-1,
+            max_iter=max_iter,
+        )
 
-  kmeans = kmeans.fit(features)
-  pred_label = kmeans.predict(features)
-  return np.float32(bipartite_match(pred_label, gt_label, n_classes).acc)
+    kmeans = kmeans.fit(features)
+    pred_label = kmeans.predict(features)
+    return np.float32(bipartite_match(pred_label, gt_label, n_classes).acc)
 
 
 def collect_results(sess, tensors, n_batches):
-  """Collects `n_batches` of tensors and aggregates the results."""
+    """Collects `n_batches` of tensors and aggregates the results."""
 
-  res = AttrDict({k: [] for k in tensors})
+    res = AttrDict({k: [] for k in tensors})
 
-  print('Collecting: 0/{}'.format(n_batches), end='')
-  for i in range(n_batches):
-    print('\rCollecting: {}/{}'.format(i + 1, n_batches), end='')
+    print('Collecting: 0/{}'.format(n_batches), end='')
 
-    vals = sess.run(tensors)
-    for k, v in vals.items():
-      res[k].append(v)
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
 
-  print('')
-  for k, v in res.items():
-    if v[0].shape:
-      res[k] = np.concatenate(v, 0)
-    else:
-      res[k] = np.stack(v)
+    for i in range(n_batches):
+        print('\rCollecting: {}/{}'.format(i + 1, n_batches), end='')
 
-  return res
+        print('')
+        vals = sess.run(tensors, options=run_options, run_metadata=run_metadata)
+        tl = timeline.Timeline(run_metadata.step_stats)
+        ctf = tl.generate_chrome_trace_format()
+        with open('timeline.json', 'w') as f:
+            f.write(ctf)
+        print('')
+
+        for k, v in vals.items():
+            res[k].append(v)
+
+    print('')
+    for k, v in res.items():
+        if v[0].shape:
+            res[k] = np.concatenate(v, 0)
+        else:
+            res[k] = np.stack(v)
+
+    return res
