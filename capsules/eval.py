2d0b94716e95bb207530777e0d088aa69d9bc7dd
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2020-08-02 22:32:37 +0800

    Update eval.py

--- capsules/eval.py
+++ capsules/eval.py
@@ -87,9 +87,11 @@ def collect_results(sess, tensors, n_batches):
 
         if i == 10:
             print('')
+            print('herehereherehere it starts')
             vals = sess.run(tensors, options=run_options, run_metadata=run_metadata)
             tl = timeline.Timeline(run_metadata.step_stats)
             ctf = tl.generate_chrome_trace_format()
+            print('herehereherehere it ends')
             with open('timeline.json', 'w') as f:
                 f.write(ctf)
             print('')
