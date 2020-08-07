4e3645577f6c5abd8cca7562c743af3c73d26bba
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2020-08-08 00:49:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -34,6 +34,8 @@ from stacked_capsule_autoencoders.capsules.eval import cluster_classify
 from stacked_capsule_autoencoders.capsules.eval import collect_results
 from stacked_capsule_autoencoders.capsules.plot import make_tsne_plot
 from stacked_capsule_autoencoders.capsules.train import tools
+from stacked_capsule_autoencoders.capsules import capsule as _capsule
+from tensorflow.python.client import timeline
 
 flags.DEFINE_string('snapshot', '', 'Checkpoint file.')
 flags.DEFINE_string(
@@ -45,55 +47,65 @@ flags.DEFINE_string('dataset', 'mnist', 'Don\'t change!')
 flags.DEFINE_string('model', 'scae', 'Don\'t change!.')
 
 
-def _collect_results(sess, tensors, dataset, n_batches):
-    """Collects some tensors from many batches."""
-
-    to_collect = AttrDict(prior_pres=tensors.caps_presence_prob,
-                          posterior_pres=tensors.posterior_mixing_probs,
-                          posterior_acc=tensors.posterior_cls_acc,
-                          prior_acc=tensors.prior_cls_acc,
-                          label=dataset['label'])
-
-    vals = collect_results(sess, to_collect, n_batches)
-    vals.posterior_pres = vals.posterior_pres.sum(1)
-    return vals
-
-
 def main(_=None):
     FLAGS = flags.FLAGS  # pylint: disable=invalid-name,redefined-outer-name
     config = FLAGS
     FLAGS.__dict__['config'] = config
-
     # Build the graph
     with tf.Graph().as_default():
 
-        model_dict = model_config.get(FLAGS)
-        data_dict = data_config.get(FLAGS)
+        dataset = tf.data.Dataset.from_tensors(tf.ones([32, 256]))
+        dataset = dataset.repeat().batch(200)
+        it = dataset.make_one_shot_iterator()
+        data_batch = it.get_next()
 
-        model = model_dict.model
-        trainset = data_dict.trainset
-        validset = data_dict.validset
+        model = _capsule.ModelTest(config.n_obj_caps,
+                                   2,
+                                   config.n_part_caps,
+                                   n_caps_params=config.n_obj_caps_params,
+                                   n_hiddens=128,
+                                   learn_vote_scale=True,
+                                   deformations=True,
+                                   noise_type='uniform',
+                                   noise_scale=4.,
+                                   similarity_transform=False)
+        #data_dict = data_config.get(FLAGS)
+        #trainset = data_dict.trainset
+        #validset = data_dict.validset
 
         # Optimisation target
-        validset = tools.maybe_convert_dataset(validset)
-        trainset = tools.maybe_convert_dataset(trainset)
-
-        valid_tensors = model(validset)
+        #validset = tools.maybe_convert_dataset(validset)
+        #trainset = tools.maybe_convert_dataset(trainset)
+        #print(validset['image'])
+        #print(trainset['image'])
+        tensor = model(data_batch)
 
         sess = tf.Session()
-        saver = tf.train.Saver()
-        saver.restore(sess, FLAGS.snapshot)
+        sess.run(tf.global_variables_initializer())
+        sess.run(tf.local_variables_initializer())
+        #saver = tf.train.Saver()
+        #saver.restore(sess, FLAGS.snapshot)
+    n_batches = 20
+    print('Collecting: 0/{}'.format(n_batches), end='')
 
-    if config.dataset == 'mnist':
-        valid_results = _collect_results(sess, valid_tensors, validset,
-                                        10000 // FLAGS.batch_size)
-    elif config.dataset == 'svhn':
-        valid_results = _collect_results(sess, valid_tensors, validset,
-                                        26032 // FLAGS.batch_size)
-    elif config.dataset == 'cifar10':
-        valid_results = _collect_results(sess, valid_tensors, validset,
-                                        10000 // FLAGS.batch_size)
+    run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+    run_metadata = tf.RunMetadata()
 
+    for i in range(n_batches):
+        print('\rCollecting: {}/{}'.format(i + 1, n_batches), end='')
+
+        if i == 10:
+            print('')
+            print('herehereherehere it starts')
+            sess.run(tensor, options=run_options, run_metadata=run_metadata)
+            tl = timeline.Timeline(run_metadata.step_stats)
+            ctf = tl.generate_chrome_trace_format()
+            print('herehereherehere it ends')
+            with open('timeline.json', 'w') as f:
+                f.write(ctf)
+            print('')
+        else:
+            sess.run(tensor)
 
 
 if __name__ == '__main__':
