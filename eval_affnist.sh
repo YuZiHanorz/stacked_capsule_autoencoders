2dc3a773a9be45a0a24b4cac7f565459a942d363
zhyu18 <38875982+YuZiHanorz@users.noreply.github.com>
2020-09-29 23:37:40 +0800

    affnist

---
+++ eval_affnist.sh
@@ -0,0 +1,37 @@
+# Copyright 2019 The Google Research Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+set -e
+set -x
+
+export PYTHONPATH=$PYTHONPATH:/work/07521/zhyu1214/maverick2/Capsule
+#source stacked_capsule_autoencoders/setup_virtualenv.sh
+python -m stacked_capsule_autoencoders.eval_mnist_model\
+  --dataset=affnist\
+  --snapshot=stacked_capsule_autoencoders/checkpoints/mnist/model.ckpt-300001\
+  --batch_size=32\
+  --canvas_size=40\
+  --n_part_caps=40\
+  --n_obj_caps=32\
+  --colorize_templates=True\
+  --use_alpha_channel=True\
+  --posterior_between_example_sparsity_weight=0.2\
+  --posterior_within_example_sparsity_weight=0.7\
+  --prior_between_example_sparsity_weight=0.35\
+  --prior_within_example_constant=4.3\
+  --prior_within_example_sparsity_weight=2.\
+  --color_nonlin='sigmoid'\
+  --template_nonlin='sigmoid'\
+  "$@"
