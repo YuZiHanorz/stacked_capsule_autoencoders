db5430f678c5b0a43d79617dc8f5f932b29fb6df
Zihan Yu <zhyu1214@login1.maverick2.tacc.utexas.edu>
2020-10-20 08:28:11 -0500

    profile

--- keras_resnet50.py
+++ keras_resnet50.py
@@ -15,18 +15,22 @@ def prepare_data_for_resnet50(data_to_transform):
     session = tf.Session()
     data = session.run(data)
     data = np.stack([data, data, data], axis=-1)
+    print(data.shape)
     return data
 
 
-(xtrain, ytrain), (xtest, ytest) = tf.keras.datasets.mnist.load_data()
-x_test = prepare_data_for_resnet50(xtest)
+(xtrain, ytrain), (xtest, ytest) = tf.keras.datasets.cifar10.load_data()
+#x_test = prepare_data_for_resnet50(xtest)
+x_test = xtest
 y_test = keras.utils.to_categorical(ytest, 10)
-x_train = prepare_data_for_resnet50(xtrain)
+#x_train = prepare_data_for_resnet50(xtrain)
+x_train = xtrain
 y_train = keras.utils.to_categorical(ytrain, 10)
+print(x_test.shape, x_train.shape)
 
-input_shape = (40, 40, 3)
+input_shape = (32, 32, 3)
 classes = 10
-bs = 100
+bs = 32
 inputs = keras.Input(shape=input_shape)
 outputs = keras.applications.ResNet50(input_shape=input_shape, weights=None, classes=classes)(inputs)
 model = keras.Model(inputs, outputs)
